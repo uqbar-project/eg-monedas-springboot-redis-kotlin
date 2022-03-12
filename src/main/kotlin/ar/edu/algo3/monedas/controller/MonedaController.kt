47a5453570c2e1343dadb4b1b7aca4130f854be7
Fernando Dodino <fernando.dodino@gmail.com>
2022-03-12 16:58:37 -0300

    Upgrading dependencies

--- src/main/kotlin/ar/edu/algo3/monedas/controller/MonedaController.kt
+++ src/main/kotlin/ar/edu/algo3/monedas/controller/MonedaController.kt
@@ -2,13 +2,9 @@ package ar.edu.algo3.monedas.controller
 
 import ar.edu.algo3.monedas.dto.Conversion
 import ar.edu.algo3.monedas.service.MonedaService
-import io.swagger.annotations.ApiOperation
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @CrossOrigin("*")
@@ -18,16 +14,16 @@ class MonedaController {
     lateinit var monedaService: MonedaService
 
     @GetMapping("/monedas")
-    @ApiOperation("Recupera información de los valores de cada moneda.")
+    @Operation(summary = "Recupera información de los valores de cada moneda.")
     fun getMonedas() = monedaService.getMonedas()
 
     @PutMapping("/monedaAPesos")
-    @ApiOperation("Convierte un valor de una moneda determinada a pesos. Para conocer la lista de monedas disponibles tenés el endpoint /GET. Se distinguen mayúsculas de minúsculas. Ejemplo: si 1 zloty está 24 pesos, al convertir 10 zlotys obtendremos 240 pesos.")
+    @Operation(summary = "Convierte un valor de una moneda determinada a pesos. Para conocer la lista de monedas disponibles tenés el endpoint /GET. Se distinguen mayúsculas de minúsculas. Ejemplo: si 1 zloty está 24 pesos, al convertir 10 zlotys obtendremos 240 pesos.")
     fun getMonedasAPesos(@RequestBody conversion: Conversion) =
         monedaService.convertirMonedaAPesos(conversion)
 
     @PutMapping("/pesosAMoneda")
-    @ApiOperation("Convierte un valor en pesos a una moneda determinada a pesos. Para conocer la lista de monedas disponibles tenés el endpoint /GET. Se distinguen mayúsculas de minúsculas. Ejemplo: si 1 zloty está 24 pesos, al convertir 12 pesos obtendremos 0.5 zlotys.")
+    @Operation(summary = "Convierte un valor en pesos a una moneda determinada a pesos. Para conocer la lista de monedas disponibles tenés el endpoint /GET. Se distinguen mayúsculas de minúsculas. Ejemplo: si 1 zloty está 24 pesos, al convertir 12 pesos obtendremos 0.5 zlotys.")
     fun getPesosAMonedas(@RequestBody conversion: Conversion) =
         monedaService.convertirPesosAMoneda(conversion)
 
--- src/test/kotlin/ar/edu/algo3/monedas/MonedaControllerTest.kt
+++ src/test/kotlin/ar/edu/algo3/monedas/MonedaControllerTest.kt
@@ -3,15 +3,12 @@ package ar.edu.algo3.monedas
 
 import ar.edu.algo3.monedas.dto.Conversion
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.mock.web.MockHttpServletResponse
